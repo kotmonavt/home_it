2a63747cfef92ddaeaf6ef4d8b41ec7cda77dc94
kotmonavt <kridtina126zz@gmail.com>
2022-09-26 21:13:53 +0300

    first test commit

---
+++ fonk.py
@@ -0,0 +1,12 @@
+def fonk(factorial):
+    print('туц-туц')
+    fonks = 1
+    i = 1
+    if factorial > 0:
+        while i <= factorial:
+            fonks *= i
+            i += 1
+    print(fonks)
+
+
+fonk(3)
